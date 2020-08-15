71b9b2e5c07008ac176df22b9db5e9835a4987f2
yanpengfei <noch1217@qq.com>
2020-08-15 17:28:34 +0800

    add Log =>serilog

--- RazorPagesMovie/Program.cs
+++ RazorPagesMovie/Program.cs
@@ -1,13 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesMovie.Models;
+using Serilog;
+using System;
 
 namespace RazorPagesMovie
 {
@@ -15,25 +12,33 @@ namespace RazorPagesMovie
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File("logs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
             // CreateHostBuilder(args).Build().Run();
             var host = CreateHostBuilder(args).Build();
 
-            using (var scope = host.Services.CreateScope())
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("App start.");
+            try
             {
-                var services = scope.ServiceProvider;
+                logger.LogInformation("App init start.");
 
-                try
-                {
-                    SeedData.Initialize(services);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
-                }
+                SeedData.Initialize(services);
             }
+            catch (Exception ex)
+            {
+
+                logger.LogError(ex, "An error occurred seeding the DB.");
+            }
+            logger.LogInformation("App init end.");
 
             host.Run();
+            logger.LogInformation("App  end.");
+
+            Log.CloseAndFlush();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -41,6 +46,8 @@ namespace RazorPagesMovie
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                });
+                })
+                .UseSerilog()
+        ;
     }
 }
--- RazorPagesMovie/Startup.cs
+++ RazorPagesMovie/Startup.cs
@@ -28,7 +28,8 @@ namespace RazorPagesMovie
             services.AddRazorPages();
 
             services.AddDbContext<RazorPagesMovieContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
+                    options.EnableSensitiveDataLogging()
+                        .UseSqlServer(Configuration.GetConnectionString("RazorPagesMovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
