7f734d3ca9f2f6ee30f1abeb40cbbaf6fb36da38
yanpengfei <noch1217@qq.com>
2020-08-15 10:25:44 +0800

    add a new field Movie.Rating

--- RazorPagesMovie/Migrations/20200815021404_Add_Movie_Rating.Designer.cs
+++ RazorPagesMovie/Migrations/20200815021404_Add_Movie_Rating.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie.Data;
 
 namespace RazorPagesMovie.Migrations
 {
     [DbContext(typeof(RazorPagesMovieContext))]
-    partial class RazorPagesMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20200815021404_Add_Movie_Rating")]
+    partial class Add_Movie_Rating
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
